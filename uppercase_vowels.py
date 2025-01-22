4d7935c1088dcccf287458af7f3f1e24f9d93457
CarloGarcellano <202480047@psu.palawan.edu.ph>
2025-01-22 19:11:47 +0800

    inital commit

--- uppercase_vowels.py
+++ uppercase_vowels.py
@@ -1,12 +1,13 @@
+
 sentence = "Python is fun and Python is easy to learn!"
+vowels = "aeiou"
+uppercasever = ""
 
+for char in sentence:
+    if char in vowels:
+        uppercasever += char.upper()
+    else:
+        uppercasever += char
 
-sentence = sentence.replace("a", "A")
-sentence = sentence.replace("e", "E")
-sentence = sentence.replace("i", "I")
-sentence = sentence.replace("o", "O")
-sentence = sentence.replace("u", "U")
-
-
-for letter in sentence:
-    print(letter, end=" ")          
+for letter in uppercasever:
+    print(letter, end=" ")
